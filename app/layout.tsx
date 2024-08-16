1c8f4f0131055142cecd448f934f53f70d75081c
Chukwunwike Udezue <udezuec@protonmail.com>
2024-08-16 23:46:35 +0100

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
+
+export const metadata: Metadata = {
+  title: "Admin - Skylink Impact",
+  description:
+    "Skylink Impact Admin: A Next.js web interface for managing the Skylink Impact system.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          inter.variable
+        )}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <div>Skylink Impact Admin</div>;
+}
