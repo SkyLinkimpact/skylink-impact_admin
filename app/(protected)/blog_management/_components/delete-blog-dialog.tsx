c8633a5274c2c78f5fc0bd2160395a656a93991e
Chukwunwike Udezue <udezuec@protonmail.com>
2024-09-02 22:27:42 +0100

    Chore: Add comments to delete blog dialog

--- app/(protected)/blog_management/_components/delete-blog-dialog.tsx
+++ app/(protected)/blog_management/_components/delete-blog-dialog.tsx
@@ -17,25 +17,65 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { Trash2 } from "lucide-react";
 import { toast } from "sonner";
 
+/**
+ * A dialog to delete a blog post.
+ *
+ * @param {Object} props
+ * @prop {Blog} props.blog - The blog post to delete.
+ * @returns {JSX.Element} The delete dialog component.
+ */
 export default function DeleteBlogDialog({ blog }: Readonly<{ blog: Blog }>) {
   const queryClient = useQueryClient();
 
+  /**
+   * The mutation to delete a blog post.
+   *
+   * @type {import("@tanstack/react-query").UseMutationResult}
+   */
   const deleteBlogMutation = useMutation({
+    /**
+     * The function to call when the mutation is triggered.
+     *
+     * @returns {Promise<void>} A promise that resolves when the blog post has been deleted.
+     */
     mutationFn: () => deleteBlog(blog.id),
+    /**
+     * The function to call when the mutation is successful.
+     */
     onSuccess: () => {
+      /**
+       * Invalidate the cache for the blog posts.
+       */
       queryClient.invalidateQueries({ queryKey: ["blogs"] });
+      /**
+       * Show a success message to the user.
+       */
       toast.success("Blog deleted", {
         position: "top-right",
       });
     },
+    /**
+     * The function to call when the mutation fails.
+     *
+     * @param {ServerErrorResponse} error - The error from the server.
+     */
     onError: (error: ServerErrorResponse) => {
+      /**
+       * Show an error message to the user.
+       */
       toast.error(error.response.data.message, {
         position: "top-right",
       });
     },
   });
 
+  /**
+   * The function to call when the user clicks the delete button.
+   */
   const handleDeleteBlog = () => {
+    /**
+     * Trigger the mutation.
+     */
     deleteBlogMutation.mutate();
   };
 
