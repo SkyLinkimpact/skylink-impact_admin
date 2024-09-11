a8e1561299b0f5365fda62b4247de7be0e07f305
Chukwunwike Udezue <udezuec@protonmail.com>
2024-09-11 22:42:15 +0100

    Fix: Node ENV error

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,11 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: (process.env.NODE_ENV === "preview" || process.env.NODE_ENV === "development") ? "https" : "http",
+        protocol:
+          process.env.NODE_ENV === "preview" ||
+          process.env.NODE_ENV === "production"
+            ? "https"
+            : "http",
         hostname: process.env.BACKEND_HOST,
       },
     ],
